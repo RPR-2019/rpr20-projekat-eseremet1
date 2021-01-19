05780253fe9a341722d9170c50b5529c0e86650b
eseremet1 <eseremet1@etf.unsa.ba>
2021-01-19 04:54:27 +0100

    Dodan index kod studenta

--- src/ba/unsa/etf/rpr/projekat/Student.java
+++ src/ba/unsa/etf/rpr/projekat/Student.java
@@ -5,10 +5,24 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Student extends User {
+    private SimpleStringProperty index;
     public Student() {
     }
 
-    public Student(SimpleIntegerProperty id, SimpleStringProperty name, SimpleStringProperty surname, SimpleStringProperty email, SimpleStringProperty username, SimpleStringProperty password, String picture) {
+    public Student(SimpleIntegerProperty id, SimpleStringProperty name, SimpleStringProperty surname, SimpleStringProperty email, SimpleStringProperty username, SimpleStringProperty password, String picture, SimpleStringProperty index) {
         super(id, name, surname, email, username, password, picture);
+        this.index = index;
+    }
+
+    public String getIndex() {
+        return index.get();
+    }
+
+    public SimpleStringProperty indexProperty() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index.set(index);
     }
 }
