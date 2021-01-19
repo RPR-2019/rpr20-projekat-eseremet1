19ecf9ef6dd81ebe275babf4a9275bf59e7812cf
eseremet1 <eseremet1@etf.unsa.ba>
2021-01-19 05:10:09 +0100

    Kreirani testovi za studenta

--- test/ba/unsa/etf/rpr/projekat/StudentTest.java
+++ test/ba/unsa/etf/rpr/projekat/StudentTest.java
@@ -2,19 +2,20 @@ package ba.unsa.etf.rpr.projekat;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class StudentTest {
 
     @Test
     void getIndex() {
-        Student user = new Student(1,"Elma","Šeremet","eseremet1@etf.unsa.ba","eseremet1","elma","");
-
-    }
-
-    @Test
-    void indexProperty() {
+        Student student = new Student(1,"Elma","Šeremet","eseremet1@etf.unsa.ba","eseremet1","elma","","18318");
+        assertEquals("18318", student.getIndex());
     }
 
     @Test
     void setIndex() {
+        Student student = new Student(1,"Elma","Šeremet","eseremet1@etf.unsa.ba","eseremet1","elma","","18318");
+        student.setIndex("10000");
+        assertEquals("10000", student.getIndex());
     }
 }
