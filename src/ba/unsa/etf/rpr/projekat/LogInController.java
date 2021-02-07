36b8036fbfcad5f56a6db12a122d170475fd4924
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-07 22:38:58 +0100

    Dodana validacija na unos praznog polja prilikom prijave

--- src/ba/unsa/etf/rpr/projekat/LogInController.java
+++ src/ba/unsa/etf/rpr/projekat/LogInController.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogInController {
+    public TextField userNameField;
+    public PasswordField passwordField;
+
+    @FXML
+    public void initialize() {
+        userNameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (userNameField.getText().trim().isEmpty()) {
+                    userNameField.getStyleClass().removeAll("poljeJeIspravno");
+                    userNameField.getStyleClass().add("poljeNijeIspravno");
+                } else {
+                    userNameField.getStyleClass().removeAll("poljeNijeIspravno");
+                    userNameField.getStyleClass().add("poljeJeIspravno");
+                }
+            }
+        });
+
+        passwordField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (passwordField.getText().trim().isEmpty()) {
+                    passwordField.getStyleClass().removeAll("poljeJeIspravno");
+                    passwordField.getStyleClass().add("poljeNijeIspravno");
+                } else {
+                    passwordField.getStyleClass().removeAll("poljeNijeIspravno");
+                    passwordField.getStyleClass().add("poljeJeIspravno");
+                }
+            }
+        });
+    }
 }
