4de815e5afb4d2c83669bc86dc61bddb35c536cf
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-07 23:57:15 +0100

    Urađen fxml za upravljanje profesorima

---
+++ src/ba/unsa/etf/rpr/projekat/AdminProffesorController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.event.ActionEvent;
+
+public class AdminProffesorController {
+    public void addProffesorAction(ActionEvent actionEvent) {
+    }
+
+    public void editProffesorAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteProffesorAction(ActionEvent actionEvent) {
+    }
+
+    public void showReportAction(ActionEvent actionEvent) {
+    }
+}
--- src/ba/unsa/etf/rpr/projekat/HomeAdminController.java
+++ src/ba/unsa/etf/rpr/projekat/HomeAdminController.java
@@ -1,9 +1,28 @@
 package ba.unsa.etf.rpr.projekat;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeAdminController {
-    public void proffesorAction(ActionEvent actionEvent) {
+    public Button proffesorBtn;
+
+    public void proffesorAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/controlProfessors.fxml"));
+        stage.setTitle("Upravljanje profesorima");
+        stage.setScene(new Scene(root, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE)); //stavljamo početni ekran
+        stage.setMinHeight(300); //da se ne može više smanjivati
+        stage.setMinWidth(200);
+        stage.show();
+        Stage stageClose = (Stage) proffesorBtn.getScene().getWindow();
+        stageClose.close();
     }
 
     public void studentAction(ActionEvent actionEvent) {
