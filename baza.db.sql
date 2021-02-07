b72f080f16fac05f34b0f4fbccd5013785c636e6
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-07 22:59:44 +0100

    Napravljena baza podataka za profesora

---
+++ baza.db.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "professor" (
+	"id"	INTEGER,
+	"name"	TEXT,
+	"surname"	TEXT,
+	"email"	TEXT,
+	"username"	TEXT,
+	"password"	TEXT,
+	PRIMARY KEY("id")
+);
+COMMIT;
