ef1ac68f7db78cdcec711a2d1d08eeeed875bdc4
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-10 02:55:24 +0100

    Urađen fxml pri izboru dodavanja materijala

---
+++ src/ba/unsa/etf/rpr/projekat/DocTypeController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.event.ActionEvent;
+
+public class DocTypeController {
+    public void uploadPDFDocumentAction(ActionEvent actionEvent) {
+
+    }
+
+    public void createWordDocumentAction(ActionEvent actionEvent) {
+
+    }
+
+    public void createTxtDocumentAction(ActionEvent actionEvent) {
+
+    }
+}
--- src/ba/unsa/etf/rpr/projekat/HomeProfessorController.java
+++ src/ba/unsa/etf/rpr/projekat/HomeProfessorController.java
@@ -2,12 +2,16 @@ package ba.unsa.etf.rpr.projekat;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
@@ -15,12 +19,15 @@ import java.util.stream.Collectors;
 public class HomeProfessorController {
 
     @FXML
-    ListView<Subject> listViewSubject;
-    SubjectDAO subjectDAO;
-    ProfessorDAO professorDAO;
+    private ListView<Subject> listViewSubject;
+    private SubjectDAO subjectDAO;
+    private ProfessorDAO professorDAO;
     private ObservableList<Subject> subjectCollection;
     private ObservableList<Professor> professorCollection;
     private Professor activeProfessor;
+    public Button addDocumentBtn;
+    public Button pdfBtn;
+    public Button profilBtn;
 
     public HomeProfessorController(Professor professor) {
         subjectDAO = SubjectDAO.getInstance();
@@ -37,10 +44,24 @@ public class HomeProfessorController {
         ArrayList<Subject> activeSubjects = subjectCollection.stream().filter(subject -> subject.getId() == activeProfessor.getSubject().getId()).collect(Collectors.toCollection(ArrayList::new));
         ObservableList<Subject> professorsSubjects = FXCollections.observableArrayList(activeSubjects);
         listViewSubject.setItems(professorsSubjects);
-
-
     }
     public void addDocumentAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/chooseDocType.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setTitle("Izbor vrste željenog materijala");
+        stage.setScene(new Scene(root, 500, 300)); //stavljamo početni ekran
+        stage.setMinHeight(300); //da se ne može više smanjivati
+        stage.setMinWidth(200);
+        stage.setResizable(false);
+
+        stage.show();
+        Stage stage1 = (Stage) addDocumentBtn.getScene().getWindow();
+        stage1.close();
 
     }
 
