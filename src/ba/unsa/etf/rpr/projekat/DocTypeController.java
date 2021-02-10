8624bdec35a07b8aaf35f837351afdde0e7d27bd
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-10 03:10:23 +0100

    Kreiran fxml za izbor word dokumenta

--- src/ba/unsa/etf/rpr/projekat/DocTypeController.java
+++ src/ba/unsa/etf/rpr/projekat/DocTypeController.java
@@ -1,6 +1,12 @@
 package ba.unsa.etf.rpr.projekat;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DocTypeController {
     public void uploadPDFDocumentAction(ActionEvent actionEvent) {
@@ -8,6 +14,18 @@ public class DocTypeController {
     }
 
     public void createWordDocumentAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/addWordDocument.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setTitle("Word document");
+        stage.setScene(new Scene(root, 500, 300)); //stavljamo početni ekran
+        stage.setMinHeight(300); //da se ne može više smanjivati
+        stage.setMinWidth(200);
+        stage.show();
 
     }
 
---
+++ src/ba/unsa/etf/rpr/projekat/WordDocumentController.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.event.ActionEvent;
+
+public class WordDocumentController {
+    public void uploadWordAction(ActionEvent actionEvent) {
+    }
+}
