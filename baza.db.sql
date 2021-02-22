e3ea89de219b415061852702cd51ceb9c1dc9f10
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-22 20:34:56 +0100

    Dodan student u bazu

--- baza.db.sql
+++ baza.db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS "professor" (
 	"username"	TEXT,
 	"password"	TEXT,
 	"professors_subject"	INTEGER,
-	"picture" TEXT,
+	"picture"	TEXT,
 	PRIMARY KEY("id"),
 	FOREIGN KEY("professors_subject") REFERENCES "subject"("id")
 );
@@ -20,15 +20,27 @@ CREATE TABLE IF NOT EXISTS "material" (
 	"id"	INTEGER,
 	"name"	TEXT,
 	"subject"	INTEGER,
-	"visibility" INTEGER,
+	"visibility"	INTEGER,
 	PRIMARY KEY("id"),
 	FOREIGN KEY("subject") REFERENCES "subject"("id")
 );
-INSERT INTO "subject" VALUES (1,'RPR');
-INSERT INTO "subject" VALUES (2,'OBP');
-INSERT INTO "subject" VALUES (3,'IM2');
-INSERT INTO "subject" VALUES (4,'AFJ');
-INSERT INTO "subject" VALUES (5,'IM1');
-INSERT INTO "subject" VALUES (6,'IF1');
-INSERT INTO "subject" VALUES (7,'IF2');
+CREATE TABLE IF NOT EXISTS "student" (
+	"id"	INTEGER,
+	"name"	TEXT,
+	"surname"	TEXT,
+	"email"	TEXT,
+	"username"	TEXT,
+	"password"	TEXT,
+	"picture"	TEXT,
+	"index"	TEXT,
+	PRIMARY KEY("id")
+);
+INSERT INTO "professor" VALUES (1,'Elma','Šeremet','eseremet1@etf.unsa.ba','eseremet1','Seremet123',2,'https://i.giphy.com/media/yFQ0ywscgobJK/giphy_s.gif');
+INSERT INTO "subject" VALUES (1,'OBP');
+INSERT INTO "subject" VALUES (2,'RPR');
+INSERT INTO "subject" VALUES (3,'AFJ');
+INSERT INTO "subject" VALUES (4,'IM1');
+INSERT INTO "material" VALUES (0,'Elma',2,1);
+INSERT INTO "material" VALUES (1,'Elma',2,1);
+INSERT INTO "material" VALUES (2,'Proba-quiz',2,1);
 COMMIT;
--- src/ba/unsa/etf/rpr/projekat/LogInController.java
+++ src/ba/unsa/etf/rpr/projekat/LogInController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -51,6 +52,14 @@ public class LogInController {
                 }
             }
         });
+
+        Tooltip toolTip = new Tooltip();
+        toolTip.setText("Password field");
+        passwordField.setTooltip(toolTip);
+        Tooltip toolTip1 = new Tooltip();
+        toolTip1.setText("Username field");
+        userNameField.setTooltip(toolTip1);
+
     }
 
     public void logInAction(ActionEvent actionEvent) throws IOException {
