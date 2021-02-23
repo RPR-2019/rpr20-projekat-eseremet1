9dbf811a6ea68e74b80484498979fe3e0946599c
eseremet1 <eseremet1@etf.unsa.ba>
2021-02-23 08:17:24 +0100

    Dodano izvjestavanje za studente

--- src/ba/unsa/etf/rpr/projekat/PrintReport.java
+++ src/ba/unsa/etf/rpr/projekat/PrintReport.java
@@ -47,7 +47,23 @@ public class PrintReport extends JFrame {
         this.setVisible(true);
     }
 
-    public void showReportStudents(Connection connection) throws JRException {
+    public void showReportStudents(Connection conn) throws JRException {
+        String reportSrcFile = getClass().getResource("/reports/ReportStudent.jrxml").getFile();
+        String reportsDir = getClass().getResource("/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JRViewer viewer = new JRViewer(print);
+        viewer.setOpaque(true);
+        viewer.setVisible(true);
+        this.add(viewer);
+        this.setSize(700, 500);
+        this.setVisible(true);
 
     }
 }
